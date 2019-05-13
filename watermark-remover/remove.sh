c567f665131de9a36cb49aa9f7c6b64540b5296f
Wei Wu <wuwei@tech-winning.com>
2019-05-13 17:54:54 +0800

    remove watermark

---
+++ watermark-remover/remove.sh
@@ -0,0 +1,1 @@
+python main.pyc --mode remove --watermark_path result/W_201_120_3.npy --alpha_path result/alpha1_201_120_3.npy --image_path demo/fotolia_168667147.jpg
---
+++ watermark-remover/train.sh
@@ -0,0 +1,5 @@
+#!/bin/python
+python3 main.pyc --mode train --dataset demo --iters 3 --watermark_threshold 0.7 --save_result true
+
+# this just for debug
+# python3 main.py --mode train --dataset demo --iters 3 --watermark_threshold 0.7 --save_result true
